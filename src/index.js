b1b76c8adba6c32a67850ff3f4d864da6a785dc3
unknown <delatorreanalia@google.com>
2023-05-18 11:28:41 -0300

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const app = require('./app');
+// const app = require('./app');
+import { listen, get } from './app';
 
 async function main() {
-    await app.listen(app.get('port'));
-    console.log("Server on port: ", app.get('port'));
+    // await app.listen(app.get('port'));
+    // console.log("Server on port: ", app.get('port'));
+    await listen(get('port'));
+    console.log("Server on port: ", get('port'));
 }
 
 main();
