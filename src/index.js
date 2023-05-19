69bb21f7ca44d8fd35885e04ca34887949e798a0
unknown <delatorreanalia@google.com>
2023-05-19 06:36:44 -0300

    Delete comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-// const app = require('./app');
-import { listen, get } from './app';
+const app = require('./app');
 
 async function main() {
-    // await app.listen(app.get('port'));
-    // console.log("Server on port: ", app.get('port'));
-    await listen(get('port'));
-    console.log("Server on port: ", get('port'));
+    await app.listen(app.get('port'));
+    console.log("Server on port: ", app.get('port'));
 }
 
 main();
